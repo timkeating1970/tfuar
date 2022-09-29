8dd79c55735800738260790e5d8f2204f4750aa5
timkeating1970 <timkeating@ameritech.net>
2022-09-29 17:17:05 -0500

    SG Test

--- web_server/main.tf
+++ web_server/main.tf
@@ -10,14 +10,34 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "web-server" {
   ami           = "ami-097a2df4ac947655f"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
-    Name = "Terraform Example"
+    Name     = "Terraform Web Server"
+    Function = "Marketing"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
